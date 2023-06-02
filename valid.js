2fe3c943301a6d7ab96b73b0e0c3d8b70556d063
Thivagaraji <114459913+Thivagaraji@users.noreply.github.com>
2023-06-02 19:45:36 +0530

    Add files via upload

---
+++ valid.js
@@ -0,0 +1,15 @@
+
+		function redirect() {
+			var email = document.getElementById('email').value;
+			var password = document.getElementById('password').value;
+			var errorMessage = document.getElementById('error-message');
+
+			if (email === 'abc@123' && password === 'password') {
+				errorMessage.textContent = ''; 
+				window.location.href = 'home.html'; 
+				return false;
+			} else {
+				errorMessage.textContent = 'Invaild Credentials'; 
+				return false; 
+			}
+		}
