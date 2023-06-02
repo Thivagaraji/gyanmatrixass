bfcdbbf0ac193093da64abf9023c95093fde0023
Thivagaraji <114459913+Thivagaraji@users.noreply.github.com>
2023-06-02 19:36:03 +0530

    Delete valid.js

--- valid.js
+++
@@ -1,9 +0,0 @@
-
-var emailInput = "test@example.com";
-var passwordInput = "password123";
-
-if (validateEmail(emailInput) && validatePassword(passwordInput)) {
-  console.log("Email and password are valid.");
-} else {
-  console.log("Email or password is invalid.");
-}
